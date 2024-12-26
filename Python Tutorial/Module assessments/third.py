af95a6b2c19975cf2ad5d931c32c3895e2aa0427
karimehabb <87314775+karimehabb@users.noreply.github.com>
2024-12-26 18:58:36 +0200

    3rd project

---
+++ Python Tutorial/Module assessments/third.py
@@ -0,0 +1,10 @@
+first_number = int(input("Enter First number :"))
+second_number = int(input("Enter second number :"))
+add = first_number + second_number
+subtract = first_number - second_number
+multip = first_number * second_number
+divid = first_number / second_number
+print("Addition = ",add)
+print("Subtraction = ",subtract)
+print("Multiplication = ",multip)
+print("Divison = ",divid)
